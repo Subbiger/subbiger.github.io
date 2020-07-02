b7e8bef3111c3b67a413dbd3efb78dbdf630bc35
Subbiger <guaralit@gmail.com>
2020-07-02 18:45:25 +0300

    new template

---
+++ Tech/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#button1").click(function(){
+      $("#tech_list1").toggleClass("tech_list2");
+    });
+    $("#button2").click(function(){
+        $("#tech_list2").toggleClass("tech_list2");
+      });
+    $("#button3").click(function(){
+        $("#tech_list3").toggleClass("tech_list2");
+      });
+  });
