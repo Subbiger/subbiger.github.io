d7f9e96582f2138b4d2ff4d56b1405f7eefbaf96
Subbiger <guaralit@gmail.com>
2020-07-06 02:58:34 +0300

    added mobile version

--- Tech/script.js
+++ Tech/script.js
@@ -9,3 +9,12 @@ $(document).ready(function(){
         $("#tech_list3").toggleClass("tech_list2");
       });
   });
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    $('.scroll_up').show(200);
+  } else {
+    $('.scroll_up').hide(200);
+  }
+}
