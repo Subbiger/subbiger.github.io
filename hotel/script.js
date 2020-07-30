61b1f1923656f7a1053139b579b5571e47526d00
Subbiger <guaralit@gmail.com>
2020-07-30 22:37:45 +0300

    updated

---
+++ hotel/script.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    $('.scroll_up').show(200);
+  } else {
+    $('.scroll_up').hide(200);
+  }
+}
